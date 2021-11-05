dda38fdae4d471d74bc0e69809dd04578cd03ae8
Miguel Aguilera <miguel.aguilera.croasdaile@gmail.com>
2021-11-05 16:53:15 -0300

    cleaned up front end a bit, currently working on cloudinary, currently image retrieval is not working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,7 +44,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :stock, :made_to_order?, :available)
+    params.require(:product).permit(:name, :description, :price, :stock, :made_to_order?, :available, :photo, :photo_url)
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts 'Cleaning database...'
 
 puts 'Creating products...'
-Product.create!(name: 'Product bear', photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Product-Bears-HD-Images.jpg', price: 1000, made_to_order?: false, stock: 10)
+Product.create!(name: 'Product bear', photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg', price: 1000, made_to_order?: false, stock: 10)
 Product.create!(name: 'Jean-Michel - Le Wagon', photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large', price: 1000, made_to_order?: false, stock: 10)
 Product.create!(name: 'Octocat -  GitHub',      photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg', price: 1000, made_to_order?: false, stock: 10)
 puts 'Creating users...'
